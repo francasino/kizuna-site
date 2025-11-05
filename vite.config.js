18a3d31143243722763046dde5f5c81f9adccc7b
Fran Casino <35731320+francasino@users.noreply.github.com>
2025-11-05 13:50:24 +0100

    Create vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// If you're NOT using a custom domain and will host at /<username>.github.io/<repo>/,
+// set base to "/kizuna-site/" (your repo name). If you WILL use a custom domain,
+// you can leave base as "/".
+export default defineConfig({
+  plugins: [react()],
+  base: "/kizuna-site/",
+});
